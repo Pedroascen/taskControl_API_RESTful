b46807f68dae3847196c84dfce064a3e514ed45e
Pedroascen <pedroascen3@gmail.com>
2024-07-03 19:24:23 -0600

    Subiendo proyecto base limpio

---
+++ src/main/java/sv/ascen2k/taskcontrol/TaskcontrolApplication.java
@@ -0,0 +1,13 @@
+package sv.ascen2k.taskcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/ascen2k/taskcontrol/TaskcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.ascen2k.taskcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
